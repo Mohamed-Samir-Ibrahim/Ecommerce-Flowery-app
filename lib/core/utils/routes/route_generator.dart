702391f773bb2106cafeb7fa548593181191b7ce
MohammedNasser12 <mohammed_abdelnasser@yahoo.com>
2025-03-26 06:15:33 +0200

    Create BottomNavScreen widget

--- lib/core/utils/routes/route_generator.dart
+++ lib/core/utils/routes/route_generator.dart
@@ -1,11 +1,11 @@
 import 'package:flowery/core/utils/routes/routes_names.dart';
 import 'package:flowery/presentation/auth/screens/signup_screen.dart';
+import 'package:flowery/presentation/bottom_navigation_bar/bottom_navigation_bar.dart';
 
 import 'package:flutter/material.dart';
 
 import '../../../presentation/auth/screens/login_screen.dart';
 import '../../../presentation/home/layout_view.dart';
-
 
 class RouteGenerator {
   static Route<dynamic> generateRoutes(RouteSettings settings) {
@@ -15,17 +15,20 @@ class RouteGenerator {
           builder: (context) => const LayoutView(),
           settings: settings,
         );
-        //-------------------------------auth
-        case RoutesNames.loginScreen:
+      //-------------------------------auth
+      case RoutesNames.loginScreen:
         return MaterialPageRoute(
-          builder:
-              (context) => LoginScreen(),
+          builder: (context) => LoginScreen(),
           settings: settings,
         );
       case RoutesNames.registerScreen:
         return MaterialPageRoute(
-          builder:
-              (context) => SignUpScreen(),
+          builder: (context) => SignUpScreen(),
+          settings: settings,
+        );
+      case RoutesNames.bottomNavScreen:
+        return MaterialPageRoute(
+          builder: (context) => BottomNavScreen(),
           settings: settings,
         );
 
