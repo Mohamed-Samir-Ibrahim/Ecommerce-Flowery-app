e982383f49c7def4c97ed04052a45f690cd3d526
Ammar-IT99 <elkingamar44@gmail.com>
2025-04-05 18:26:22 +0200

    Delete auth related files under signup directory.

--- lib/domain/repository_contract/auth_repository_contract/signup/signup_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flowery/domain/common/api_result.dart';
-import 'package:flowery/domain/entity/auth_entity/signup/signup_entity.dart';
-
-abstract class SignupRepository{
-  Future<ApiResult<SignupEntity>>SignUp(String firstName , String lastName , String email,String password,String rePassword ,String phone,String gender);
-
-
-}
