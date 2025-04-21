189ac119361532d1d26233a414839ed46d618020
Mohamed Samir <mohamed.samir.ibrahim98@gmail.com>
2025-04-21 20:41:43 +0200

    fix: adding unit testing.

---
+++ test/mocks/mock_about_repository.dart
@@ -0,0 +1,5 @@
+import 'package:flowery/domain/use_case/Profile_use_case/about_use_case.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([AboutUseCase])
+void main() {}
---
+++ test/mocks/mock_terms_repository.dart
@@ -0,0 +1,5 @@
+import 'package:flowery/domain/use_case/Profile_use_case/terms_use_case.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([TermsUseCase])
+void main() {}
